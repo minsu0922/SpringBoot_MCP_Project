5079cb0cd4b9ff162bae19e9a021a010dab0e741
minsu <minsu092274@naver.com>
2026-02-18 21:22:16 +0900

    Initial project setup

---
+++ src/main/java/com/spring/public_web/PublicWebApplication.java
@@ -0,0 +1,13 @@
+package com.spring.public_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PublicWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PublicWebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/spring/public_web/PublicWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.public_web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PublicWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
